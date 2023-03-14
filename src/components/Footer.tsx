2ee4db5f4e44e07e6abb47399561cd573d0cef64
Mario Abdala <mario_abdala@hotmail.com>
2023-03-14 10:05:37 -0300

    first steps in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,29 @@
 import './Footer.css';
 import { FooterColumn } from './FooterColumn';
 
-export const Footer = ({...props}) => {
+export const Footer = () => {
     return (
-    <div className="footer" style={props.style}>
+    <div className="footer">
         <div className="footer-contact">
-            
+            <h3>NANINA INDUMENTARIA</h3>
+            <div className="footer-icon-dual-text-container">
+                <span className="material-icons footer-icon">place</span>
+                <div className="location">
+                    <p>San Martín 575</p>
+                    <p>Villaguay, Entre Ríos</p>
+                </div>
+            </div>
+            <div className="footer-icon-text-container">
+                <span className="material-icons footer-icon">mail_outline</span>
+                <a className="footer-link" href="mailto:naninaindumentaria@gmail.com">naninaindumentaria@gmail.com</a>
+            </div>
+            <div className="footer-icon-text-container">
+                <span className="material-icons footer-icon">phone_iphone</span>
+                <p>03455 422297</p>
+            </div>
+            <div className="footer-logos-container">
+                <a className="footer-logo-container" href="https://www.instagram.com/nanina_lenceria/"><img className="footer-logo" src="https://cdn.iconscout.com/icon/free/png-256/instagram-1464521-1239436.png" alt="instagram logo" /></a>
+            </div>
         </div>
         <FooterColumn title={"Categorías"} links={[["Corpiños","/productos/corpiños"], ["Bombachas", "/productos/bombachas"]]} />
     </div>
